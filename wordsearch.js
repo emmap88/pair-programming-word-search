6f40a92b5d53475da4961c4bd4d0660ca9be77d2
emmap88 <emmap8@hotmail.com>
2023-08-22 16:02:35 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true; // if it contains the word, return true
+  }
+  
+  const verticalJoin = [];
+ 
+  for (let col = 0; col < letters.length; col++) { // Create the for loop
+    let verticalWord = ''; // Create the string to push the letters into
 
-module.exports = wordSearch
+    for (let row = 0; row < letters.length; row++) { // Nested loop to swap over the rows to col
+      verticalWord += letters[row][col];
+    }
+    verticalJoin.push(verticalWord); // Push the string into the array
+    
+    for (let l of verticalJoin) {
+      if (l.includes(word)) return true; // if it contains the word, return true
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
